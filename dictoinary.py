f514f7a86aa03d8639db7860adeeb728bb8e16e9
WilliamLi0430 <WilliamL355@nycstudents.net>
2025-10-29 09:16:06 -0400

    Update dictoinary.py

--- dictoinary.py
+++ dictoinary.py
@@ -1,55 +1,56 @@
 items = [
     {   
         "name": "Luke Zheng's shirt",
-        "price": "$9.99",
+        "price": "9.99",
         "category": "Clothing",
         "stock": "One"
     },
     {
         "name": "Johnny Yuan's shoes",
-        "price": "$69.99",
+        "price": "69.99",
         "category": "Clothing",
         "stock": "Two"
     }, 
     {
         "name": "Andrew Lee's hair",
-        "price": "$14.99",
+        "price": "14.99",
         "category": "Costumes",
         "stock": "One"
     },
     {
         "name": "Ryan Chen's backpack",
-        "price": "$44.99",
+        "price": "44.99",
         "category": "Bags",
         "stock": "One"
     },
     {
         "name": "41-inch longsword",
-        "price": "$149.99",
+        "price": "149.99",
         "category": "Weapons",
         "stock": "Twelve"
     },
     {
         "name": "EasternBlueJay",
-        "price": "$1.99",
+        "price": "1.99",
         "category": "Discord username", 
         "stock": "One"
     },
     {
         "name": "Github Desktop",
-        "price": "$5,000",
+        "price": "5000",
         "category": "Online Services",
         "stock": "Ten Thousand"
     },
     {
         "name": "Tencent",
-        "price": "$722,000,000,000",
+        "price": "722000000000",
         "category": "Gaming",
         "stock": "One"
     }
 ]
 
 cart = []
+cost = 0
 e = 1
 while e == 1:
     for index, item in enumerate(items):
@@ -57,6 +58,7 @@ while e == 1:
     a = int(input("What item(s) do you want to buy? (0-7) "))
     cart.append(items[a]["price"])
     cart.append(items[a]["name"])
+    cost += (float(items[a]["price"]))
     b = input("Do you wish to continue? ")
     if b == ("yes") or b == ("Yes"):
         e += 0
@@ -64,7 +66,7 @@ while e == 1:
         print(cart)
         t = input("Are you sure you want to buy these items? ")
         if t == ("yes") or t == ("Yes"):
-            print("ha")
+            print(cost)
             break
         else:
             print("Restarting...")
